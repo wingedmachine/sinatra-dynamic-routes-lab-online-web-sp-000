91b4448f5846479389319144f71a6d0732edc62c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-15 21:35:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do\
-    "#{params[:phrase]*params[:number]}"
+    "#{params[:phrase]*params[:number].to_i}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
